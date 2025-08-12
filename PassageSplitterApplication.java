37332d16e2972fef991b80048b810bbb5f78c0b6
lazyvamp <91076236+lazyvamp16@users.noreply.github.com>
2025-08-12 15:58:18 +0530

    Create PassageSplitterApplication.java

---
+++ PassageSplitterApplication.java
@@ -0,0 +1,11 @@
+package com.example.passagesplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassageSplitterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PassageSplitterApplication.class, args);
+    }
+}
